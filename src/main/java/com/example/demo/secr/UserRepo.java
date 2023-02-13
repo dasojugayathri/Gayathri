d707172ed0d60b85bb89c09d72f85322ec312923
dasojugayathri <dasojugayathri3@gmail.com>
2023-02-13 15:23:57 +0530

    changes applied

--- src/main/java/com/example/demo/secr/UserRepo.java
+++ src/main/java/com/example/demo/secr/UserRepo.java
@@ -16,6 +16,6 @@ public interface UserRepo  extends JpaRepository<OurUser, Integer>{
   
   @Query(("select r from OurUser u join u.roles r where u.uname=?1"))
   public abstract List<Role> getRoles(String userName);
-  //hello hii
+  //hii
 }
 
